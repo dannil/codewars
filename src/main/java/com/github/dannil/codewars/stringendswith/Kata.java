67fea8f77e1bde12ce07da576aabca80cde3550c
Daniel Nilsson <7290072+dannil@users.noreply.github.com>
2025-02-14 21:07:58 +0100

    String ends with?

---
+++ src/main/java/com/github/dannil/codewars/stringendswith/Kata.java
@@ -0,0 +1,7 @@
+package com.github.dannil.codewars.stringendswith;
+
+public class Kata {
+    public static boolean solution(String str, String ending) {
+        return str.endsWith(ending);
+    }
+}
