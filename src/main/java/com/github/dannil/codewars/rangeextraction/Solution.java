32643cf17ac3f712f62129fb4940df8691b97a80
Daniel Nilsson <7290072+dannil@users.noreply.github.com>
2025-03-01 18:49:26 +0100

    Range Extraction

---
+++ src/main/java/com/github/dannil/codewars/rangeextraction/Solution.java
@@ -0,0 +1,11 @@
+package com.github.dannil.codewars.rangeextraction;
+
+class Solution {
+    public static String rangeExtraction(int[] arr) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            int current = arr[i];
+            if ()
+        }
+    }
+}
